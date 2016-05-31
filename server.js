5533919d970d9a144e020909abec0353e36a5c27
SBRKS <sbrks3@gmail.com>
2016-05-31 09:06:16 -0700

    updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//note to annabelle/instructors: i didn't get far on this this weekend--I was away, and when I did begin I hit a few road bumps--but want to keep working on it on my own over the next week
+
 // require express and other modules
 var express = require('express'),
     app = express();
@@ -6,6 +8,13 @@ var express = require('express'),
 // and populate the req.body object
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//json bucket list items
+
+var bucketList = [{
+  //add bucket list objects
+
+}];
 
 /************
  * DATABASE *
@@ -37,17 +46,62 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
+    woops_i_has_forgot_to_document_all_my_endpoints: false, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/sbrks/express_self_api/README.md", // CHANGE ME
+    base_url: "http://sampi.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/bucket", description: "Gets a bucket list"},
+      {method: "GET", path: "/api/profile", description: "Data about me"},
+      {method: "POST", path: "/api/bucket", description: "Create a new bucket list item"},
+      {method: "PUT", path: "/api/bucket", description: "Edit a bucket list item"},
+      {method: "DELETE", path: "/api/bucket", description: "Remove a bucket list item"},
     ]
   })
 });
+
+  app.get('/api/profile', function api_profile (req, res) {
+    res.json({
+      name: "Sam Brooks",
+      github_link: "https://github.com/sbrks",
+      github_profile_image: "urlofgithubprofileimage", //change me
+      current_city: "San Francisco, California",
+      pets: "None, but am determined to get a rescue dog very soon"
+    })
+  });
+
+
+
+  app.get('/api/bucket', function api_bucket(req, res) {
+        res.json({
+        bucketItem: "X",
+        Deadline: "X"
+    })
+  });
+
+    app.post('/api/bucket', function api_bucket(req, res) {
+        res.json({
+        bucketItem: "X",
+        Deadline: "X"
+    })
+  });
+
+
+      app.put('/api/bucket', function api_bucket(req, res) {
+        res.json({
+        bucketItem: "X",
+        Deadline: "X"
+    })
+  });
+
+
+    app.delete('/api/bucket', function api_bucket(req, res) {
+        res.json({
+        bucketItem: "X",
+        Deadline: "X"
+        })
+  });
 
 /**********
  * SERVER *
